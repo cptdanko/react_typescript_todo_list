9b91cecec0c94567ea081aa6abaddb76b9fbd35b
Bhuman Soni <bhuman.soni@gmail.com>
2023-01-07 14:52:31 +1100

    added a media query to optimise for mobile screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import './App.css';
 import { TodoList } from './components/todolist';
+// import './customMuiComponents/custom.css';
 import { Login } from './components/login';
-import { Typography } from '@mui/material';
+import { Card, Typography } from '@mui/material';
 import { Dashboard } from './containers/dashboard';
 
 function App() {
 
   return (
     <div className="App">
-      
-      <div style={{paddingTop: 10}}>
-          <div className='TodoArea'>
-            <Typography sx={{ fontSize: 18 }} color="text.secondary" gutterBottom>
-                React todo list with Typescript & MUI
-            </Typography>
-            <Dashboard />
-          </div>
-      </div>
+      <div className='TodoArea'>
+        <Card className='CustomCard'>
+          <Typography sx={{ fontSize: 18, paddingTop: 2 }} color="text.secondary" gutterBottom>
+            React todo list with Typescript & MUI
+          </Typography>
+          <hr />
+          <TodoList />
+          </Card>
+        </div>
     </div>
   );
 }
