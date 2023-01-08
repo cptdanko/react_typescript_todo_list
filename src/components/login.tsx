692121e95c7e086beefc467367893339f8cbf009
Bhuman Soni <bhuman.soni@gmail.com>
2023-01-09 09:10:11 +1100

    Added tabs, removed unused inputs, refactored dialogs to their own file

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,93 +1,93 @@
-import { Button, Card, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider } from "@mui/material";
+import {
+  Card,
+  Divider,
+} from "@mui/material";
 import { gapi } from "gapi-script";
 import { useEffect, useState } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
+import { SimpleDialog } from "./dialogs/simpleDialog";
 
 export const Login = (props: any) => {
-    const { setShowTodo, setUsername } = props; 
-    //oauth
-    const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? '';
-    const [showLogout, setShowLogout] = useState<boolean>(false);
-    const [showGLogin, setShowGLogin] = useState<boolean>(true);
-    const [openDialog, setOpenDialog] = useState<boolean>(false);
-    const [loginDialogMsg, setLoginDialogMsg] = useState<string>('');
-    const [loginDialogTitle, setLoginDialogTitle] = useState<string>('');
-    const SUCCESSFULL_LOGIN_MSG = "You are logged in using your Google ID";
-    const FAILED_LOGIN_MSG = "Login with Google ID failed";
+  const { setShowTodo, setUsername } = props;
+  //oauth
+  const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? "";
+  const [showLogout, setShowLogout] = useState<boolean>(false);
+  const [logoutMsg, setLogoutMsg] = useState<string>("");
+  const [showGLogin, setShowGLogin] = useState<boolean>(true);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [loginDialogMsg, setLoginDialogMsg] = useState<string>("");
+  const [loginDialogTitle, setLoginDialogTitle] = useState<string>("");
+  const SUCCESSFULL_LOGIN_MSG = "You are logged in using your Google ID";
+  const FAILED_LOGIN_MSG = "Login with Google ID failed";
 
-    useEffect(() => {
-        const initClient = () => {
-            gapi.client.init({
-                clientId: GOOGLE_CLIENT_ID,
-                scope: ''
-            });
-        }
-        gapi.load('client:auth2', initClient);
-    });
-
-    const onSuccess = (res: any) => {
-        const userProfile = res.profileObj;
-        if (userProfile) {
-            setLoginDialogMsg(SUCCESSFULL_LOGIN_MSG);
-            setUsername(userProfile.givenName);
-            setLoginDialogTitle('Hi '+ userProfile.givenName);
-            setShowTodo(true);
-            setShowLogout(true);
-            setShowGLogin(false);
-            setOpenDialog(true);
-        }
-    }
-
-    const onFailure = (res: any) => {
-        setLoginDialogMsg(`${FAILED_LOGIN_MSG} - ${res.error}`);
-        setLoginDialogTitle('Login Failed');
-        setOpenDialog(true);
-    }
-    const logout = () => {
-        setShowLogout(false);
-        setShowGLogin(true);
-        setOpenDialog(false);
-        setLoginDialogTitle('Logged out');
-        setLoginDialogMsg('Logout successful');
-        setOpenDialog(true);
-    }
-    const handleClose = () => {
-        setOpenDialog(false);
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: GOOGLE_CLIENT_ID,
+        scope: "",
+      });
     };
+    gapi.load("client:auth2", initClient);
+  });
 
-    return (
-        <Card>
-            {showGLogin ? <GoogleLogin clientId={GOOGLE_CLIENT_ID}
-                buttonText="Sign in with Google"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                isSignedIn={true}
-                cookiePolicy={'single_host_origin'}
-            /> : ''
-            }
-            <Divider />
-            {showLogout ? <GoogleLogout onLogoutSuccess={logout}
-                clientId={GOOGLE_CLIENT_ID} />
-                : ''}
+  const onSuccess = (res: any) => {
+    const userProfile = res.profileObj;
+    if (userProfile) {
+      setLoginDialogMsg(SUCCESSFULL_LOGIN_MSG);
+      setUsername(userProfile.givenName);
+      setLoginDialogTitle("Hi " + userProfile.givenName);
+      setLogoutMsg(`Logout ${userProfile.givenName}`);
+      setShowTodo(true);
+      setShowLogout(true);
+      setShowGLogin(false);
+      setOpenDialog(true);
+    }
+  };
 
-            <Dialog
-                open={openDialog}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {loginDialogTitle}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        {loginDialogMsg}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Ok</Button>
-                </DialogActions>
-            </Dialog>
-        </Card>
-    )
-}
+  const onFailure = (res: any) => {
+    setLoginDialogMsg(`${FAILED_LOGIN_MSG} - ${res.error}`);
+    setLoginDialogTitle("Login Failed");
+    setOpenDialog(true);
+  };
+  const logout = () => {
+    setShowLogout(false);
+    setShowGLogin(true);
+    setOpenDialog(false);
+    setLoginDialogTitle("Logged out");
+    setLoginDialogMsg("Logout successful");
+    setOpenDialog(true);
+  };
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+
+  return (
+    <Card>
+      {showGLogin ? (
+        <GoogleLogin
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Sign in with Google"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          isSignedIn={true}
+          cookiePolicy={"single_host_origin"}
+        />
+      ) : (
+        ""
+      )}
+      <Divider />
+      {showLogout ? (
+        <GoogleLogout buttonText={logoutMsg} onLogoutSuccess={logout} clientId={GOOGLE_CLIENT_ID} />
+      ) : (
+        ""
+      )}
+
+      <SimpleDialog
+        openDialog={openDialog}
+        handleClose={handleClose}
+        dialogHeader={loginDialogTitle}
+        dialogMessage={loginDialogMsg}
+      />
+    </Card>
+  );
+};
--- src/containers/dashboard.tsx
+++ src/containers/dashboard.tsx
@@ -1,55 +1,45 @@
-import { Button, Card, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { Button, Card } from "@mui/material";
 import { gapi } from "gapi-script";
-import { useState } from "react"
+import { useState } from "react";
+import { SimpleDialog } from "../components/dialogs/simpleDialog";
 import { Login } from "../components/login";
-import { TodoList } from "../components/todolist";
+import { TabLayout } from "./tabLayout";
 
 /**
  * The Dashboard container which will have either show the login component
  * or the todo list component with a logout option
  */
 export const Dashboard = () => {
-    const [showTodo, setShowTodo] = useState(false);
-    const [username, setUsername] = useState("");
-    const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [showTodo, setShowTodo] = useState(false);
+  const [username, setUsername] = useState("");
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
-    const showTodoForm = () => {
-        if (!gapi.auth.getToken()) {
-            setOpenDialog(true);
-        }
+  const showTodoForm = () => {
+    if (!gapi.auth.getToken()) {
+      setOpenDialog(true);
     }
+  };
 
-    const handleClose = () => {
-        setOpenDialog(false);
-        setShowTodo(!showTodo);
-    }
+  const handleClose = () => {
+    setOpenDialog(false);
+    setShowTodo(!showTodo);
+  };
 
-    return (
-        <Card>
-            <Login setShowTodo={setShowTodo} setUsername={setUsername} />
+  return (
+    <Card>
+      <Login setShowTodo={setShowTodo} setUsername={setUsername} />
 
-            {showTodo ?
-                <TodoList /> : <Button onClick={showTodoForm}> Add todo list</Button>
-            }
-
-            <Dialog
-                open={openDialog}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {'Limited functionality'}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        {'You can get a feel of how to app is, but it will have limited functionality'}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Ok</Button>
-                </DialogActions>
-            </Dialog>
-        </Card>
-    )
-}
+      {showTodo ? (
+        <TabLayout username={username} />
+      ) : (
+        <Button onClick={showTodoForm}> Add todo list</Button>
+      )}
+      <SimpleDialog
+        openDialog={openDialog}
+        handleClose={handleClose}
+        dialogHeader="Limited functionality"
+        dialogMessage="You can get a feel of how to app is, but it will have limited functionality"
+      />
+    </Card>
+  );
+};
